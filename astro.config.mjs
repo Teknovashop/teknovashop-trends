bcbd2596a46610204a69888874a624f8246bf595
Teknovashop <teknovashop25@gmail.com>
2025-09-03 19:33:36 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/edge';
 
 export default defineConfig({
   site: process.env.PUBLIC_SITE_URL || 'https://teknovashop-trends.vercel.app',
   output: 'hybrid',
-  adapter: vercel(),
+  adapter: vercel()
 });
